27f9290d2afe6d367c7e707a39693147d3fadda2
Pratibha Nimbolkar <pratibha.nimbolkar@diggibyte.com>
2024-04-17 11:50:44 +0530

    employee_bronze_to_silver

---
+++ src/assignment1/bronze_to_silver/employee_bronze_to_silver.py
@@ -0,0 +1,73 @@
+# Databricks notebook source
+# MAGIC %run /Users/pratibha.nimbolkar@diggibyte.com/assignment1/source_to_bronze/utils
+
+# COMMAND ----------
+
+from pyspark.sql.types import StructType, StructField, IntegerType, StringType
+custom_schema = StructType([
+    StructField("Emp_Id", IntegerType(), True),
+    StructField("Emp_Name", StringType(), True),
+    StructField("department", StringType(), True),
+    StructField("country", StringType(), True),
+    StructField("salary", IntegerType(), True),
+    StructField("age", IntegerType(), True)
+])
+
+emp_path = f'dbfs:/FileStore/assignments/assign1/source_to_bronze/employee.csv'
+employee_df = custom_schema_read_csv(emp_path , schema= custom_schema)
+
+employee_df.show()
+employee_df.printSchema()
+
+
+
+# COMMAND ----------
+
+schema = StructType([
+    StructField("dep_Id", StringType(), True),
+    StructField("dep_Name", StringType(), True)
+])
+
+dep_path = 'dbfs:/FileStore/assignments/assign1/source_to_bronze/department.csv/part-00000-tid-2882255029493062016-d1ad3e95-7576-478f-8622-a6c4dd54a8c7-34-1-c000.csv'
+department_df = custom_schema_read_csv(dep_path, schema)
+department_df.printSchema()
+department_df.show()
+
+
+# COMMAND ----------
+
+schema = StructType([
+    StructField("countryCode", StringType(), True),
+    StructField("countryName", StringType(), True)
+])
+country_path = 'dbfs:/FileStore/assignments/assign1/source_to_bronze/country.csv/part-00000-tid-1152614239620934396-b87d3132-1b94-40d1-8bd1-bd170c96fc85-35-1-c000.csv'
+country_df = spark.read.csv(country_path,schema)
+country_df.printSchema()
+country_df.show()
+
+# COMMAND ----------
+
+employee_snake_case = camel_to_snake(employee_df)
+display(employee_snake_case)
+
+
+
+# COMMAND ----------
+
+date_load = current_date_df(employee_snake_case)
+display(date_load)
+
+# COMMAND ----------
+
+
+ spark.sql('CREATE DATABASE employee_info')
+ spark.sql('use employee_info')
+
+
+
+# COMMAND ----------
+
+employee_df.write.option('path', 'dbfs:/FileStore/assignments/questoin1/silver/employee_info/dim_employee').saveAsTable('dim_employee')
+
+# COMMAND ----------
+
