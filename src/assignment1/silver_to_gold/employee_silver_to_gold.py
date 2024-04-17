f76b7da892a6240334c011ca7f5bcd06aeff3353
Pratibha Nimbolkar <pratibha.nimbolkar@diggibyte.com>
2024-04-17 11:50:55 +0530

    employee_silver_to_gold

---
+++ src/assignment1/silver_to_gold/employee_silver_to_gold.py
@@ -0,0 +1,38 @@
+# Databricks notebook source
+# MAGIC %run /Users/pratibha.nimbolkar@diggibyte.com/assignment1/source_to_bronze/utils8/+
+
+# COMMAND ----------
+
+employee_df = spark.read.format("delta").load('dbfs:/FileStore/assignments/questoin1/silver/employee_info/dim_employee')
+display(employee_df)
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+ display(salary_of_each_department(employee_df))
+
+# COMMAND ----------
+
+display(employee_count(employee_df))
+
+# COMMAND ----------
+
+display(list_the_department(employee_df))
+
+# COMMAND ----------
+
+display(avg_age(employee_df))
+
+# COMMAND ----------
+
+display(add_load_date(employee_df))
+
+# COMMAND ----------
+
+employee_df.write.format("parquet").mode("overwrite").option("replaceWhere","load_date = '2024-04-16'").save("/FileStore/assignments/gold/employee/table_name")
+
+# COMMAND ----------
+
